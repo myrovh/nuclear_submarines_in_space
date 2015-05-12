fb4af96848e48e9b56b96cbb0c41b53859d9f7bc
myrovh <myrovh@yandex.com>
2015-05-13 01:06:27 +1000

    initial unity scene
    planet orbits sun with basic gravity effect and init force push to
    simulate orbit

---
+++ Assets/scripts/Planet.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Planet : MonoBehaviour
+{
+    public Transform orbit_target; 
+    public int orbit_speed;
+
+    private Rigidbody planet_physics;
+
+    void Start()
+    {
+        planet_physics = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        transform.LookAt(orbit_target);
+        while (orbit_speed > 0)
+        {
+            planet_physics.AddForce(transform.right * orbit_speed);
+            orbit_speed = 0;
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        if (planet_physics != null)
+        {
+            Gizmos.color = Color.red;
+            Vector3 direction = planet_physics.velocity;
+            Gizmos.DrawRay(transform.position, direction);
+        }
+    }
+}
