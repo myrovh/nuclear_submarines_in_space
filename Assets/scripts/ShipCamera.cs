77bb6e86c415bc25667e457d626d9528c03d8f57
myrovh <myrovh@yandex.com>
2015-05-14 01:41:15 +1000

    implemented free look camera on key press

--- Assets/scripts/ShipCamera.cs
+++ Assets/scripts/ShipCamera.cs
@@ -6,13 +6,30 @@ public class ShipCamera : MonoBehaviour
 {
     public bool IsFreeLook = false;
     public Transform Target;
+    public float XSensitivity;
+    public float YSensitivity;
 
+    private float rotationX;
+    private float rotationY;
+
+    //TODO change isfreelook to use a function. on function call store rotation and fix it in place so camera rotates around that location instead of the object
     private void Update()
     {
         if (!IsFreeLook)
         {
             transform.LookAt(Target);
+            transform.parent.rotation = Target.rotation;
             transform.rotation = Target.rotation;
+        }
+        else
+        {
+            rotationX += Input.GetAxis("Mouse X")*XSensitivity;
+            rotationY += Input.GetAxis("Mouse Y")*YSensitivity;
+
+            Quaternion xQuaternion = Quaternion.AngleAxis(rotationX, Vector3.up);
+            Quaternion yQuaternion = Quaternion.AngleAxis(rotationY, Vector3.right);
+
+            transform.parent.localRotation = yQuaternion*xQuaternion;
         }
     }
 }
---
+++ Assets/scripts/UserInput.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UserInput : MonoBehaviour
+{
+    public GameObject PlayerObject;
+    private Ship _playerShip;
+    public GameObject CameraObject;
+    private ShipCamera _cameraScript;
+
+    void Start()
+    {
+        _playerShip = PlayerObject.GetComponent<Ship>();
+        _cameraScript = CameraObject.GetComponent<ShipCamera>();
+    }
+
+	void Update ()
+    {
+        #region Camera Controls
+        //Turns on free look while user presses 'c' key
+        if (Input.GetKeyDown(KeyCode.C))
+	    {
+	        _cameraScript.IsFreeLook = true;
+	    }
+
+        //Disables free look when user releases 'c' key
+	    if (Input.GetKeyUp(KeyCode.C))
+	    {
+	        _cameraScript.IsFreeLook = false;
+        }
+        #endregion
+    }
+}
