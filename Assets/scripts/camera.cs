e37cff07b50d577dfebefa63cc70378e55afc3fc
myrovh <myrovh@yandex.com>
2015-05-14 00:34:59 +1000

    added basic 3rd person camera
    free look not implemented

---
+++ Assets/scripts/camera.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class camera : MonoBehaviour
+{
+    public Transform Target;
+    public bool is_free_look = false;
+
+    private void Update()
+    {
+        if (!is_free_look)
+        {
+            transform.LookAt(Target);
+            transform.rotation = Target.rotation;
+        }
+        else
+        {
+            //free look camera functions
+        }
+    }
+}
